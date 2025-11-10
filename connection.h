e86809afa179864864f4a5b8ddd29af4ba9ebaf4
RezzyQ <nlreyf202@gmail.com>
2025-11-10 23:03:37 +0300

    Create connection.h

---
+++ connection.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "errno.h"
+#include "crypto.h"
+#include "interface.h"
+#include <system_error>
+#include <netinet/in.h>
+#include <memory>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <iostream>
+#include <vector>
+#include <unistd.h>
+#include <fstream>
+#include <vector>
+using namespace std;
+#define BUFFER_SIZE 1024
+
+class Connection{
+private:
+    static string salt;
+public:
+    static int conn(const Params* p);
+};
